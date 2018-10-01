06a92d3601691980fd53a89b5350f7bd24dd95fc
vdice <vadice@microsoft.com>
2018-10-01 17:25:42 -0600

    add ghNotify to brigade.js

--- brigade.js
+++ brigade.js
@@ -1,19 +1,28 @@
-const { events, Job } = require("brigadier");
+const { events, Job, Group } = require("brigadier");
 
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.revision.commit)
 
-  run_tests()
+  test_runner.run()
 })
 
 events.on("pull_request", function(e, project) {
   var parsedPayload = JSON.parse(e.payload)
   console.log("received a pull request event with the url " + parsedPayload.pull_request.url)
 
-  run_tests()
+  start = ghNotify("pending", `Build started as ${ e.buildID }`, e, project)
+
+  // Run tests in parallel. Then if it's a release, push binaries.
+  // Then send GitHub a notification on the status.
+  Group.runAll([start, test_runner])
+  .then(() => {
+      return ghNotify("success", `Build ${ e.buildID } passed`, e, project).run()
+   }).catch(err => {
+    return ghNotify("failure", `Failed build ${ e.buildID }`, e, project).run()
+  });
 })
 
-function run_tests() {
+function test_runner() {
   // Create a new job
   var node = new Job("test-runner")
 
@@ -27,7 +36,19 @@ function run_tests() {
     "cd /src/",
     "python setup.py test"
   ]
+  return node
+}
 
-  // We're done configuring, so we run the job
-  node.run()
+function ghNotify(state, msg, e, project) {
+  const gh = new Job(`notify-${ state }`, "technosophos/github-notify:latest")
+  gh.env = {
+    GH_REPO: project.repo.name,
+    GH_STATE: state,
+    GH_DESCRIPTION: msg,
+    GH_CONTEXT: "brigade",
+    GH_TOKEN: project.secrets.ghToken,
+    GH_COMMIT: e.revision.commit,
+    GH_TARGET_URL: `http://104.41.134.224/kashti/builds/${ e.buildID }`,
+  }
+  return gh
 }
